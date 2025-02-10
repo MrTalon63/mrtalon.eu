865d3b618d74d2d528dacc1421f635ed38fb7de0
Marcin Czop <marcin@mrtalon.eu>
2025-02-10 22:35:08 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,4 +22,9 @@
 			</div>
 		</div>
 	</div>
+	<div class="invisible">
+		<a href="https://www.abuseipdb.com/user/55917" title="AbuseIPDB is an IP address blacklist for webmasters and sysadmins to report IP addresses engaging in abusive behavior on their networks">
+			<img src="https://www.abuseipdb.com/contributor/55917.svg" alt="AbuseIPDB Contributor Badge" style="width: 50px;">
+		</a>
+	</div>
 </body>
